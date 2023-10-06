05952f3ec5aedab43c71f88a487c01457fcc19d7
Marcelo-Nobre <joaomarceloviana6@gmail.com>
2023-10-06 17:32:11 -0300

    [perf]:Adicionei um grupo de rotas pra deshboard

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Web\QuizController;
-
+use App\Http\Controllers\Admin\QuizController as QuizControllerAdmin;
 
 
 
@@ -23,9 +22,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::get('/dashboard', [QuizControllerAdmin::class,'index'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
